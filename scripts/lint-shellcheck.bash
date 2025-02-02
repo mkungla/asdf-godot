90bb2a70047c0f0dd2718f52f7f693a834c66ac4
Marko Kungla <marko@mkungla.dev>
2025-02-02 20:18:50 +0200

    devel: setup shfmt and shellcheck
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- scripts/lint-shellcheck.bash
+++ scripts/lint-shellcheck.bash
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-shellcheck --shell=bash --external-sources \
-	bin/* --source-path=template/lib/ \
-	lib/* \
-	scripts/*
+find bin scripts lib -type f -exec shellcheck --shell=bash --external-sources --source-path=lib {} +
