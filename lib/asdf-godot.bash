4cb1fb76b170c64e558479e0652fc89a18bd0785
Marko Kungla <marko@mkungla.dev>
2025-02-02 20:51:22 +0200

    feat: add latest-stable script
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- lib/asdf-godot.bash
+++ lib/asdf-godot.bash
@@ -1,3 +1,34 @@
 #!/usr/bin/env bash
 
-echo "create new godot game"
+TOOL_NAME="godot"
+TOOL_TEST="godot --version"
+
+GODOT_STABLE_REPO="https://github.com/godotengine/godot"
+
+curl_opts=(-fsSL)
+# NOTE: You might want to remove this if godot is not hosted on GitHub releases.
+if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+fi
+
+fail() {
+	echo -e "asdf-$TOOL_NAME: $*"
+	exit 1
+}
+
+sort_versions() {
+	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+list_github_tags() {
+	git ls-remote --tags --refs "$GODOT_STABLE_REPO" |
+		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+		sed 's/^v//'
+}
+
+list_all_versions() {
+	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
+	# Change this function if godot has other means of determining installable versions.
+	list_github_tags
+}
