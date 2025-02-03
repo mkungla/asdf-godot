df8f7ade3246851af0a20e382199ca045d13431c
Marko Kungla <marko@mkungla.dev>
2025-02-03 10:43:23 +0200

    fix: cp destination
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- lib/asdf-godot.bash
+++ lib/asdf-godot.bash
@@ -99,7 +99,7 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "${ASDF_DOWNLOAD_PATH}/*" "$install_path"
+		cp -r "${ASDF_DOWNLOAD_PATH}"/* "$install_path"
 
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
